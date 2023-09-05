01248e11d9ec04e91cdff3f770f10a75a4886bb3
Ahkuu <Dylantwhite89@yahoo.com>
2023-09-05 07:55:15 +0100

    Add files via upload

---
+++ day_2_Datatypes.py
@@ -0,0 +1,29 @@
+# Data Types
+
+# Strings
+# You CAN add a square bracket to determine where in a string you want to pull that character from
+# For example in the word hello. if I put [0] the H will get printed
+
+print("Hello"[4])
+
+
+# Integer is used for whole numbers without any decimal places
+
+# You can add _ instead of commas for very large numbers
+print(123 + 456)
+
+
+# Float another data type. It is short for a floating point number used for decimal place numbers
+
+
+# Boolean, another data type. It only has 2 possible outcomes. true of false
+
+True
+False
+
+# Always used with a capital letter
+
+a = float(1.23)
+# You can use the 'type' data type to help you find out what kind of data type is present
+# you can change a data type and turn it into a string by putting inbetween the parenthesis of a str() data type
+print(type(a))
